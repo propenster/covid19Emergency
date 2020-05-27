cc5b97392860e766f6018c7f6770845260440f44
propenster <51266654+propenster@users.noreply.github.com>
2020-05-27 02:50:32 +0100

    Added the main scripts....

---
+++ contact-us.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html> 
+<html lang="en">
+	
+<!-- doccure/blank-page.html  30 Nov 2019 04:12:20 GMT -->
+<head>
+		<meta charset="utf-8">
+		<title>Doccure</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+		
+		<!-- Favicons -->
+		<link href="assets/img/favicon.png" rel="icon">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		
+		<!-- Fontawesome CSS -->
+		<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+		<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+		
+		<!-- Main CSS -->
+		<link rel="stylesheet" href="assets/css/style.css">
+		
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+			<script src="assets/js/html5shiv.min.js"></script>
+			<script src="assets/js/respond.min.js"></script>
+		<![endif]-->
+	
+	</head>
+	<body>
+
+		<!-- Main Wrapper -->
+		<div class="main-wrapper">
+		
+			<!-- Header -->
+			<?php include('inc/header.php'); ?>
+			<!-- /Header -->
+			
+			<!-- Breadcrumb -->
+			<div class="breadcrumb-bar">
+				<div class="container-fluid">
+					<div class="row align-items-center">
+						<div class="col-md-12 col-12">
+							<nav aria-label="breadcrumb" class="page-breadcrumb">
+								<ol class="breadcrumb">
+									<li class="breadcrumb-item"><a href="index.php">Home</a></li>
+									<li class="breadcrumb-item active" aria-current="page">Contact</li>
+								</ol>
+							</nav>
+							<h2 class="breadcrumb-title">Contact Us</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- /Breadcrumb -->
+			
+			<!-- Page Content -->
+			<div class="content">
+				<div class="container">
+					<div class="row">
+						<div class="col-12">
+							<h4 class="card-title">Get In Touch with Us</h4>
+								<form>
+								<div class="row">
+								<div class=" col-lg-6 col-md-12">
+								<div class="form-group">
+								<input type="text" class="form-control" placeholder="Full name" required />
+								</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class=" col-lg-6 col-md-12">
+								<div class="form-group">
+								<input type="email" class="form-control" placeholder="Email address" required />
+								</div>
+								</div>
+							</div>
+
+							<div class="row">
+								<div class=" col-lg-6 col-md-12">
+								<div class="form-group">
+								<input type="text" class="form-control" placeholder="Subject" required />
+								</div>
+								</div>
+							</div>
+							
+						<div class="row">
+								<div class=" col-lg-6 col-md-12">
+								<div class="form-group">
+								<textarea class="form-control" placeholder="Messege" rows="7" required></textarea>
+								</div>
+								</div>
+								</div>
+								<div class="submit-section">
+								<button type="submit" class="btn btn-info mb-3 btn-lg">Send</button>
+								<button type="reset" class="button button-rounded  close">&times;</button>
+							</div>
+								</form>
+								
+						</div>
+					</div>
+				</div>
+
+			</div>		
+			<!-- /Page Content -->
+   
+			<!-- Footer -->
+			<?php include('inc/footer.php'); ?>
+			<!-- /Footer -->
+		   
+		</div>
+		<!-- /Main Wrapper -->
+	  
+		<!-- jQuery -->
+		<script src="assets/js/jquery.min.js"></script>
+		
+		<!-- Bootstrap Core JS -->
+		<script src="assets/js/popper.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		
+		<!-- Custom JS -->
+		<script src="assets/js/script.js"></script>
+		
+	</body>
+
+<!-- doccure/blank-page.html  30 Nov 2019 04:12:20 GMT -->
+</html>
